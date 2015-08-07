14d7b17f9d2124e1c98749ede1f3659b26b9ce45
ririzarr <rairizarry@gmail.com>
2015-08-07 00:07:55 -0400

    adding shell scripts

---
+++ makechapter.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+printf "  *** knit file, fix latex, and move to  *** \n\n"
+
+cd $1
+Rscript --no-init-file -e "library(knitr); knit('$2.Rmd')"
+
+sed 's/\$\$/@@@@/g' $2.md |
+    sed 's/ \$/ @@@@/g' |
+    sed 's/\$ /@@@@ /g' |
+    sed 's/\$\./@@@@\./g' |
+    sed 's/\$,/@@@@,/g' |
+    sed 's/\$:/@@@@:/g' |
+    sed 's/\$-/@@@@-/g' |
+    sed 's/(\$/(@@@@/g' |
+    sed 's/\$)/@@@@)/g' |
+    sed 's/^\$/@@@@/g' |
+    sed 's/\$$/@@@@/g' |
+    sed 's/@@@@/\$\$/g' |
+    sed 's/\$\$/{\$\$}/g' |
+    sed 's/(figure\//(images\//g' > $2.md
+    
+awk '
+BEGIN {count = 0;}
+{
+gsub(/\{\$\$\}/," \{\$\$\} ");
+for (i=1;i<=NF;++i){ 
+	if($i=="\{\$\$\}") {
+		++count;
+		if(count==1){ printf "%s", "\{\$\$\}"} else { printf "%s", "\{/\$\$\}"; count=0} 
+} else {printf "%s ", $i}
+}
+printf "\n";
+} 
+' $2.md > ../manuscript/$2.md
+
+rm $2.md
+cd ..
+
+imgcount=`ls -1 $1/figure/$2* 2> /dev/null | wc -l`
+ 
+if [ $imgcount -gt 0 ]
+then
+mv $1/figure/$2* manuscript/images
+fi
+
+
+printf "  *** add new files to BOOK, commit and push *** \n\n"
+
+cd manuscript
+git add $2.md
+
+if [ $imgcount -gt 0 ]
+then
+git add images/$2*
+fi
+
+git commit -am "adding $2 to book"
+git push 
+
+printf "\n  *** done! *** \n\n"
+
