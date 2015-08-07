4652db8c59bd94a4a1c8dbb5e72f0bda8adfb4be
ririzarr <rairizarry@gmail.com>
2015-08-07 08:38:33 -0400

    adding shell scripts and other stuff

---
+++ makebook.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## Run makechapter on each line
+while read line; do
+	if [ "$line" != "" ]; then
+		if [[ $line != *"#"* ]]; then
+			set -- $line;
+			./makechapter.sh $1 $2; 
+		fi
+	fi
+done < filenames.txt 
+
+
+## create the Book.txt file
+while read line; do
+	if [ "$line" != "" ]; then
+		if [[ $line != *"#"* ]]; then
+			set -- $line;
+			echo $2.md;
+		else
+			echo $line;
+		fi
+	else
+		printf "\n";
+	fi
+done < filenames.txt > manuscript/Book.txt
