093b3374a3dea0f5747fd8319c0664bc5dca8fff
ririzarr <rairizarry@gmail.com>
2015-08-30 15:50:26 -0400

    added imagess

--- downloadimages.sh
+++ downloadimages.sh
@@ -4,18 +4,25 @@ cd manuscript
 mkdir -p images/downloads
 for f in *.md;
 do
+	## Find all the figures that are a url
+	## form is ![foo](http::bar)
+	## awk extracts just the link
 	result=$(grep "\!\[.*](http" $f | awk -F'[()]' '{print $2}')
 	for theurl in $result
 	do
- 		cd images/downloads
-		wget $theurl
-	 	cd ../..
-	 	filename="${theurl##*/}"
+		##filename is just the last part of the url
+ 		filename="${theurl##*/}"
+	 	cd images/downloads
+		if [ ! -f $filename ]; then
+  			wget $theurl
+	 	fi
+		cd ../..
 	 	filename="images/downloads/$filename"
+		## now that we have downloaded, replace 
 	 	sed  "s#$theurl#$filename#" < $f > "$f.tmp"
 	 	mv "$f.tmp" $f 
  	done
 done
 cd ..
 git add manuscript/images/downloads/*
-git commit -am "added imagess"; git push
+git commit -am "added images"; git push
