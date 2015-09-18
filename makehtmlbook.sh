3017c7b53ac46fd9f426a58cdcf7e0a82949321d
Mike Love <michaelisaiahlove@gmail.com>
2015-09-17 22:12:10 -0400

    makehtmlbook

--- makehtmlbook.sh
+++ makehtmlbook.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-## Run makechapter on each line
+## run knit and add2book on each line
 while read line; do
     if [ "$line" != "" ]; then
 	if [[ $line != *"#"* ]]; then
 	    if [[ $line != *"exercises"* ]]; then
+		echo " --- now processing ---"
+		echo $line
+		echo ""
 		cd ../book
+		set -- $line;
 		./knit.sh $1 $2;
 		./add2book.sh $1 $2;
 		cd ../leanpub
